c962410c6313a804dc155239e791a074f25c822b
Ryan Remer <remerrd@byu.edu>
2017-11-21 18:49:25 -0700

    json

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -6,4 +6,20 @@ function mainCtrl($scope,$http){
 	$scope.currentChoice = {};
 	$scope.currentIndex = 0;
 	$scope.nextIndex = 0;	
+
+	$scope.postChoice = function(){
+		var newChoice = {
+			title: $scope.title,
+			desc: $scope.desc,
+			options: $scope.options.toLowerCase().split(','),
+			paths: []
+		};
+
+		for (let i = 0; i < newChoice.options.length;i++){
+			newChoice.paths.push(0);
+			newChoice.options[i] = newChoice.options[i].trim();
+		}
+
+		$scope.jsonChoice = JSON.stringify(newChoice);
+	}
 }
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
- var server_world = [];
- var first_choice = {
-        title: "A New Land",
-        desc: 
-        `You open your eyes and find yourself at a crossroad of two dirt roads
-        To the North you see a great mountain range with snow at the top of the peaks.
-        To the East you see a forest filled with life with a mysterious aura.
-        To the West you see a great Medieval Castle, with the farmland and homes along the path.
-        To the South you see a coastline with a large boat in the distance`,
-        options: ["North", "East", "West", "South"],
-        paths: [0,0,0,0]
-  }
-  server_world.push(first_choice);
+var newChoice = {
+  title: req.body.title,
+  desc: req.body.desc,
+  options: req.body.options,
+  paths: req.body.paths};
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -28,14 +21,8 @@ router.get('/world', function(req, res) {
 router.post('/world', function(req,res){
   console.log("In post world")
   console.log(req.body);
-  var newChoice = {
-    title: req.body.title,
-    desc: req.body.desc,
-    options: req.body.options,
-    paths: req.body.paths};
   
-  server_world[req.body.my_index].paths[req.body.my_path] = server_world.length;
-  server_world.push(newChoice);
+  
   
   res.end('{"success" : "Updated Successfully", "status" : 200}');
 });
