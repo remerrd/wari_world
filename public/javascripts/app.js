4c73bcd59b13fe66868c2e9648e7657691a6ff5d
Ryan Remer <remerrd@byu.edu>
2017-11-21 23:34:04 -0700

    style

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,6 +7,24 @@ function mainCtrl($scope,$http){
 	$scope.world = [];
 	$scope.currentIndex = "";
 	$scope.nextIndex = "";
+	$scope.createInfo = "";
+
+	$scope.createInfo = function(){
+		if (scope.createInfo == ""){
+			scope.createInfo = `
+			You are the first person to have chosen this, now its up to you to continue the story. To do this fill out the information below.
+			Have a title for the situation, then describe the situation and possible choices, last give key words the user will need to type seperated by a ",".
+			
+			Some basic tips,
+			1) include a transition from where you are comming from (last area will be displayed below)
+			2) use as much detail as you can, so people can live the story
+			3) if you have played on other outcomes, reference people and places from other paths
+			`
+		}
+		else{
+			scope.createInfo = ""
+		}
+	}
 
 	$scope.chooseChoice = function(){
 		console.log($scope.input);
